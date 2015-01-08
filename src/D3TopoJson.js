4fe80beb217b01d2e004a94d500f0db35884bb0d
Michael Pruett <michael@mapsense.co>
2015-01-07 18:45:11 -0600

    Allow changing the function used to extract GeoJSON features from TopoJSON.

--- src/D3TopoJson.js
+++ src/D3TopoJson.js
@@ -1,7 +1,7 @@
 po.d3TopoJson = function(fetch) {
   if (!arguments.length) fetch = po.queue.json;
 
-  var topoToGeo = function(url, callback) {
+  var topologyFeatures = function(topology) {
     function convert(topology, object, layer, features) {
       if (object.type == 'GeometryCollection' && !object.properties) {
         object.geometries.forEach(function(g) {
@@ -20,19 +20,29 @@ po.d3TopoJson = function(fetch) {
       }
     }
 
+    var features = [];
+    for (var o in topology.objects) {
+      convert(topology, topology.objects[o], o, features);
+    }
+    return features;
+  };
+
+  var topoToGeo = function(url, callback) {
     return fetch(url, function(topology) {
-      var features = [];
-      for (var o in topology.objects) {
-        convert(topology, topology.objects[o], o, features);
-      }
       callback({
         type: 'FeatureCollection',
-        features: features
+        features: topologyFeatures(topology)
       });
     });
   };
 
   var d3TopoJson = po.d3GeoJson(topoToGeo);
+
+  d3TopoJson.topologyFeatures = function(x) {
+    if (!arguments.length) return topologyFeatures;
+    topologyFeatures = x;
+    return d3TopoJson;
+  };
 
   return d3TopoJson;
 };
